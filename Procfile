6191e6684bff5bff3181cb9d26e194df4acc404c
jack.teruya <jack.teruya@gmail.com>
2022-03-24 09:52:51 -0400

    feat/add django-celery-beat and add template index and add api coins

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: gunicorn quote_management.wsgi --log-file -
 worker: python manage.py celery worker
-celery_beat: python manage.py celery beat
+celery_beat: celery -A quote_management beat --scheduler django_celery_beat.schedulers:DatabaseScheduler
--- core/api_view/rates.py
+++ core/api_view/rates.py
@@ -3,19 +3,30 @@ import datetime
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from core.models import Rates, Coin
 from core.serializers.serializers import RatesSerializers
 from core.utils.api_vatcompy import ServiceUpdateCoin
 
 
-class RatesApiViews(ListAPIView):
+class RatesListApiViews(ListAPIView):
+
+    # ´v1/ap1/rates/´  -- List rates
+
+    queryset = Rates.objects.order_by("-date_rate").all()
+    serializer_class = RatesSerializers
+
+
+class RateApiViews(RetrieveAPIView):
 
     # ´v1/ap1/rates/´  -- List rates
 
     queryset = Rates.objects.all()
     serializer_class = RatesSerializers
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 
 class CreateIntialRatesApiViews(APIView):
@@ -28,26 +39,8 @@ class CreateIntialRatesApiViews(APIView):
     serializer_class = RatesSerializers
 
     def get(self, request, format=None, *args, **kwargs):
-        coins = Coin.objects.all()
-        if coins:
-            coins = [coin.sigla for coin in Coin.objects.all()]
-        else:
-            real = Coin()
-            real.name = "Real"
-            real.sigla = "BRL"
-            real.save()
 
-            iene = Coin()
-            iene.name = "Iene"
-            iene.sigla = "JPY"
-            iene.save()
-
-            euro = Coin()
-            euro.name = "Euro"
-            euro.sigla = "EUR"
-            euro.save()
-
-            coins = [coin.sigla for coin in Coin.objects.all()]
+        coins = self._get_coin()
 
         for count in range(kwargs['days']):
             date = (datetime.datetime.today() - datetime.timedelta(days=count))
@@ -70,3 +63,29 @@ class CreateIntialRatesApiViews(APIView):
                 serializer.save()
 
         return Response({"status code": 200, "message": "rates created successfully"})
+
+    def _get_coin(self):
+
+        coins = Coin.objects.all()
+
+        if coins:
+            coins = [coin.sigla for coin in Coin.objects.all()]
+        else:
+            real = Coin()
+            real.name = "Real"
+            real.sigla = "BRL"
+            real.save()
+
+            iene = Coin()
+            iene.name = "Iene"
+            iene.sigla = "JPY"
+            iene.save()
+
+            euro = Coin()
+            euro.name = "Euro"
+            euro.sigla = "EUR"
+            euro.save()
+
+            coins = [coin.sigla for coin in Coin.objects.all()]
+
+        return coins
